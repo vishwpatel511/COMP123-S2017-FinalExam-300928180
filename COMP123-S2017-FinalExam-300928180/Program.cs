052a6addf96bcae43c8beecf8db64eaa37d14f10
Vishw Patel <vishwpatel511@gmail.com>
2017-08-17 12:46:35 -0400

    Add project files.

---
+++ COMP123-S2017-FinalExam-300928180/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+/*
+ * Name: Vishw Patel
+ * Student ID: 300928180
+ * Date: July 17, 2017
+ * Description: This is the Suit enum
+ * Version: 0.1 - Created the Suit enum
+ */
+
+namespace COMP123_S2017_FinalExam_300928180
+{
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new PickHighestCardForm());
+        }
+    }
+}
---
+++ COMP123-S2017-FinalExam-300928180/Suit.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Vishw Patel
+ * Student ID: 300928180
+ * Date: July 17, 2017
+ * Description: This is the Suit enum
+ * Version: 0.1 - Created the Suit enum
+ */
+
+namespace COMP123_S2017_FinalExam_300928180
+{
+    public enum Suit
+    {
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+}
